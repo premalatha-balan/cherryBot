d62f5d6f312cb736b2a2823cba85681faade89df
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2023-04-16 17:41:44 +0000

    apikey button and apikey working

--- cherrybot/src/pages/index.js
+++ cherrybot/src/pages/index.js
@@ -1,10 +1,50 @@
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      Hello World
+  const [apiKey, setApiKey] = useState('');
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+
+  async function sendRequest() {
+    
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer ' + apiKey
+      },
+      body: JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Hello!"}]
+      })
+    });
+
+    const responseJson = await response.json();
+    console.log('responseJson', responseJson);
+  }
+ 
+
+  return <div className="flex flex-col h-screen"> 
+    <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+      <div className="text-xl font-bold">CherryBot</div>
+      <div>
+        <input 
+        type="password" 
+        className="border p-2 rounded"  
+        onChange={e => setApiKey(e.target.value)}
+        value={apiKey}
+        placeholder="Paste API key here"/>
+      </div>
+    </nav>
+
+    <div className="p-4" >
+      <button 
+      className="border rounded-md p-2 bg-red-300 hover:bg-pink-400 text-white"
+      onClick={sendRequest}>
+        Send Request
+        </button>
     </div>
-   )
+
+  </div>
 }
 
 
